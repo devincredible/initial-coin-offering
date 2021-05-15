027ece6d85ae0050c160a146f9253b615620a3cd
leonsuzuki0719 <leonsuzuki0719@gmail.com>
2021-05-15 09:57:03 +0200

    create a new solidity file with the crowdsale smart-contract

---
+++ contracts/TokenCrowdale.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.4.24;
+
+// Crowdsale
+// Timed Crowdsale
+// Capped Crowdsale
+// Minted Crowdsale
+// Whitelisted Crowdsale
+// Refundable Crowdsale
+// Presale/Public Sale
+//Token Distribution & Vesting
+
+import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+
+contract TokenCrowdsale {
+
+}

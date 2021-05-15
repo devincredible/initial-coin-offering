c8d65afc47cf663f45d790e613bdca8213398fed
leonsuzuki0719 <leonsuzuki0719@gmail.com>
2021-05-15 10:31:06 +0200

    add a constructor to the Crowdsales smart-contract

--- contracts/TokenCrowdale.sol
+++ contracts/TokenCrowdale.sol
@@ -11,6 +11,16 @@ pragma solidity 0.4.24;
 
 import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 
-contract TokenCrowdsale {
+contract TokenCrowdsale is Crowdsale {
+    
+    constructor(
+        uint256 _rate, 
+        address _wallet, 
+        ERC20 _token
+    ) 
+        Crowdsale(_rate, _wallet, _token)
+        public 
+    {
 
+    }
 }

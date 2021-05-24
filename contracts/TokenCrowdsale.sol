cfaaf58cffd6e137e50718f38eed74f262f4726a
leonsuzuki0719 <leonsuzuki0719@gmail.com>
2021-05-24 09:57:58 +0200

    add Min Cap and Max Cap to the smart contact

--- contracts/TokenCrowdsale.sol
+++ contracts/TokenCrowdsale.sol
@@ -11,17 +11,33 @@ pragma solidity 0.4.24;
 
 import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
 import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
 
-contract TokenCrowdsale is Crowdsale, MintedCrowdsale {
+contract TokenCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale {
+    
+    uint256 public investorMinCap = 2000000000000000; // Minimum investor contribution - 0.002 Ether
+    uint256 public investorMaxCap = 50000000000000000000; // Maximum investor contribution - 50 Ether
+    mapping(address => uint256) public contributions;
     
     constructor(
         uint256 _rate, 
         address _wallet, 
-        ERC20 _token
+        ERC20 _token,
+        uint256 _cap
     ) 
         Crowdsale(_rate, _wallet, _token)
+        CappedCrowdsale(_cap)
         public 
     {
 
     }
+
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+        super._preValidatePurchase(_beneficiary, _weiAmount);
+        uint256 _existingContribution = contributions[_beneficiary];
+        uint256 _newContribution = _existingContribution.add(_weiAmount);
+        require(_newContribution >= investorMinCap && _newContribution <= investorMaxCap);
+        contributions[_beneficiary] = _newContribution;
+    }
+
 }
--- test/TokenCrowdsale.test.js
+++ test/TokenCrowdsale.test.js
@@ -21,45 +21,53 @@ contract('TokenCrowdsale', ([_, _wallet, investor1, investor2]) => {
     // TokenCrowdsale config
     const rate = 500;
     const wallet = _wallet;
+    const cap = ether(100);
 
     beforeEach(async () => {
         token = await Token.new(name, symbol, decimals); // Deploy Token
-        crowdsale = await TokenCrowdsale.new(rate, wallet, token.address) // Deploy TokenCrowdsale
+        crowdsale = await TokenCrowdsale.new(rate, wallet, token.address, cap) // Deploy TokenCrowdsale
         await token.transferOwnership(crowdsale.address);
     });
 
-    describe('crowdsale', () => {
-        it('tracks the rate', async () => {
-            const _rate = await crowdsale.rate();
-            _rate.toString().should.equal(rate.toString());
-        });
+    // describe('crowdsale', () => {
+    //     it('tracks the rate', async () => {
+    //         const _rate = await crowdsale.rate();
+    //         _rate.toString().should.equal(rate.toString());
+    //     });
 
-        it('tracks the wallet', async () => {
-            const _wallet = await crowdsale.wallet();
-            _wallet.toString().should.equal(wallet.toString());
-        });
+    //     it('tracks the wallet', async () => {
+    //         const _wallet = await crowdsale.wallet();
+    //         _wallet.toString().should.equal(wallet.toString());
+    //     });
 
-        it('tracks the token', async () => {
-            const _token = await crowdsale.token();
-            _token.should.equal(token.address);
-        });
-    });
+    //     it('tracks the token', async () => {
+    //         const _token = await crowdsale.token();
+    //         _token.should.equal(token.address);
+    //     });
+    // });
 
-    describe('minted crowdsale', () => {
-        it('mints tokens after purchase', async() => {
-            const originalTotalSupply = await token.totalSupply();
-            await crowdsale.sendTransaction({ value: ether(1), from: investor1 });
-            const newTotalSupply = await token.totalSupply();
-            assert.isTrue(newTotalSupply > originalTotalSupply);
-        });
-    });
+    // describe('minted crowdsale', () => {
+    //     it('mints tokens after purchase', async() => {
+    //         const originalTotalSupply = await token.totalSupply();
+    //         await crowdsale.sendTransaction({ value: ether(1), from: investor1 });
+    //         const newTotalSupply = await token.totalSupply();
+    //         assert.isTrue(newTotalSupply > originalTotalSupply);
+    //     });
+    // });
+
+    describe('capped crowdsale', () => {
+        it('has the correct cap', async () => {
+            const _cap = await crowdsale.cap();
+            _cap.toString().should.equal(cap.toString());
+        })
+    })
     
-    describe('accepting payments', () => {
-        it('should accept payments', async () => {
-            const value = ether(1);
-            const purchaser = investor2;
-            await crowdsale.sendTransaction({ value: value, from: investor1 }).should.be.fulfilled;
-            await crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
-        });
-    });
+    // describe('accepting payments', () => {
+    //     it('should accept payments', async () => {
+    //         const value = ether(1);
+    //         const purchaser = investor2;
+    //         await crowdsale.sendTransaction({ value: value, from: investor1 }).should.be.fulfilled;
+    //         await crowdsale.buyTokens(investor1, { value: value, from: purchaser }).should.be.fulfilled;
+    //     });
+    // });
 });    

caadc68e51303d324c9bdf33f5207358d0e0bbfb
leonsuzuki0719 <leonsuzuki0719@gmail.com>
2021-06-05 10:01:23 +0200

    modify the file '2_deploy_crowdsale.js' to deploy to the development newtwork. Modify also the truffle-config file to be able to deploy to the ropsten test network

--- migrations/2_deploy_crowdsale.js
+++ migrations/2_deploy_crowdsale.js
@@ -1,9 +1,43 @@
 const Token = artifacts.require("./Token.sol");
+const TokenCrowdsale = artifacts.require("./TokenCrowdsale");
 
-module.exports = function (deployer) {
+function ether (n) {
+    return new web3.utils.BN(
+        web3.utils.toWei(n.toString(),'ether')
+    );
+}
+
+const duration = {
+    seconds: function (val) { return val; },
+    minutes: function (val) { return val * this.seconds(60); },
+    hours: function (val) { return val * this.minutes(60); },
+    days: function (val) { return val * this.hours(24); },
+    weeks: function (val) { return val * this.days(7); },
+    years: function (val) { return val * this.days(365); },
+  };
+
+module.exports = async function (deployer, network, accounts) {
     const _name = "mTC Token";
     const _symbol = "mTC";
     const _decimals = 18;
     
-    deployer.deploy(Token, _name, _symbol, _decimals);
+    await deployer.deploy(Token, _name, _symbol, _decimals);
+    const deployedToken = await Token.deployed();
+
+    const latestTime = (new Date).getTime();
+    
+    const _rate             = 500;
+    const _wallet           = accounts[0];
+    const _token            = deployedToken.address;
+    const _openingTime      = latestTime + duration.minutes(30);
+    const _closingTime      = _openingTime + duration.weeks(1);
+    const _cap              = ether(100);
+    const _goal             = ether(50);
+    const _foundersFund     = accounts[0];
+    const _foundationFund   = accounts[0];
+    const _partnersFund     = accounts[0];
+    const _releaseTime      = _closingTime + duration.days(1);
+    
+    await deployer.deploy(TokenCrowdsale, _rate, _wallet, _token, _cap, _openingTime, _closingTime, _goal, _foundersFund, _foundationFund, _partnersFund, _releaseTime);
+    return true;
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,10 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+
+
 
 module.exports = {
   networks: {
@@ -7,6 +12,17 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" //match any network id
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/${process.env.INFURA_API_KEY}`
+        );
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 3
     }
   },
   compilers: {
